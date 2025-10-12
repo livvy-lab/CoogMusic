c43d472e042ba77fb1f0fa6cfcb463e2b531b380
Liv <olivianwright11@gmail.com>
2025-10-12 17:32:58 -0500

    Initial React + Node.js + MySQL setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import pool from './db.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  const [rows] = await pool.query('SELECT * FROM users');
+  res.json(rows);
+});
+
+app.listen(3001, () => console.log('Server running on port 3001'));
