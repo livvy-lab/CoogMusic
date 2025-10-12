ecd7f9967cfee61b88ade0acd112e94850e071d3
Liv <olivianwright11@gmail.com>
2025-10-12 17:50:04 -0500

    edited db.js

--- server/db.js
+++ server/db.js
@@ -1,12 +1,15 @@
-import mysql from 'mysql2/promise';
-import dotenv from 'dotenv';
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-const pool = mysql.createPool({
+const db = await mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
-export default pool;
+console.log("✅ Connected to MySQL");
+
+export default db;
