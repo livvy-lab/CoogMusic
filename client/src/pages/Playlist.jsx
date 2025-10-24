387be6041db095e1785f9434742f9d53328d24ee
Liv <olivianwright11@gmail.com>
2025-10-24 13:50:12 -0500

    playlists page

---
+++ client/src/pages/Playlist.jsx
@@ -0,0 +1,10 @@
+import PageLayout from "../components/PageLayout/PageLayout";
+import PlaylistCard from "../components/PlaylistGrid/PlaylistGrid";
+
+export default function Playlist() {
+  return (
+    <PageLayout>
+        <PlaylistCard></PlaylistCard>
+    </PageLayout>
+  );
+}
