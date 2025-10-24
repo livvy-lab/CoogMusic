ab2f7fc5189d51548cc7fb39baa890adf77acfb4
Vinny DyCruz <vdycruz@gmail.com>
2025-10-24 13:07:20 -0500

    fixing formatting of registeration

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import leftRecord from "../assets/left_record.svg";
-import rightRecord from "../assets/right_record.svg";
-import earbuds from "../assets/earbuds.svg";
 import "./Auth.css";
+import Loading from "../components/LoadingLayout/Loading";
 
 export default function Register() {
   const [form, setForm] = useState({
@@ -14,6 +12,7 @@ export default function Register() {
     user: "",
     password: "",
   });
+
   const navigate = useNavigate();
   const set = (k) => (e) => setForm((f) => ({ ...f, [k]: e.target.value }));
 
@@ -22,47 +21,80 @@ export default function Register() {
     navigate("/login");
   };
 
-  return (
-    <div className="authShell">
-      <img src={leftRecord} alt="" className="bg bg-left-record" />
-      <img src={rightRecord} alt="" className="bg bg-right-record" />
-      <img src={earbuds} alt="" className="bg bg-earbuds" />
+  const onCancel = () => navigate("/login");
 
+  return (
+    <Loading>
       <div className="authCard authCardLarge">
         <div className="authChip">LISTENER REGISTRATION</div>
 
         <form className="regGrid" onSubmit={onSubmit}>
           <label className="regField">
             <span>First name:</span>
-            <input className="authInput" value={form.first} onChange={set("first")} />
+            <input
+              className="authInput"
+              value={form.first}
+              onChange={set("first")}
+            />
           </label>
           <label className="regField">
             <span>Last name:</span>
-            <input className="authInput" value={form.last} onChange={set("last")} />
+            <input
+              className="authInput"
+              value={form.last}
+              onChange={set("last")}
+            />
           </label>
 
           <label className="regField">
             <span>Major:</span>
-            <input className="authInput" value={form.major} onChange={set("major")} />
+            <input
+              className="authInput"
+              value={form.major}
+              onChange={set("major")}
+            />
           </label>
           <label className="regField">
-            <span>Minor(optional):</span>
-            <input className="authInput" value={form.minor} onChange={set("minor")} />
+            <span>Minor (optional):</span>
+            <input
+              className="authInput"
+              value={form.minor}
+              onChange={set("minor")}
+            />
           </label>
 
           <label className="regField">
             <span>User:</span>
-            <input className="authInput" value={form.user} onChange={set("user")} />
+            <input
+              className="authInput"
+              value={form.user}
+              onChange={set("user")}
+            />
           </label>
           <label className="regField">
             <span>Password:</span>
-            <input className="authInput" type="password" value={form.password} onChange={set("password")} />
+            <input
+              className="authInput"
+              type="password"
+              value={form.password}
+              onChange={set("password")}
+            />
           </label>
 
           <div className="regActions">
-            <button type="button" className="authBtn authBtnGhost">Cancel</button>
-            <button type="button" className="authBtn authBtnChip">profile pic</button>
-            <button type="submit" className="authBtn authBtnPrimary">Sign Up</button>
+            <button
+              type="button"
+              className="authBtn authBtnGhost"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+            <button type="button" className="authBtn authBtnChip">
+              profile pic
+            </button>
+            <button type="submit" className="authBtn authBtnPrimary">
+              Sign Up
+            </button>
           </div>
         </form>
 
@@ -73,6 +105,6 @@ export default function Register() {
           </Link>
         </p>
       </div>
-    </div>
+    </Loading>
   );
 }
