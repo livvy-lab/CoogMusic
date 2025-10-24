2d1c714aa91902b07497c5b724974f453229b9f9
Liv <olivianwright11@gmail.com>
2025-10-23 20:11:15 -0500

    pagelayout fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ListenerHead from "./components/ListenerProfileistenerprofile/ListenerHead";
 
 const listener = {
   FirstName: "Olivia",
@@ -11,8 +10,4 @@ const listener = {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <ListenerHead listener={listener} />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
